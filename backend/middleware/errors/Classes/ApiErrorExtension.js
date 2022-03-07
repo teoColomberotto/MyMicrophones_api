ea0fe6ed1cb81c5db2851dd94108ca4743cc067a
Teo Colomberotto <teocolomberotto@gmail.com>
2022-03-07 23:04:12 +0100

    add ApiError class

---
+++ backend/middleware/errors/Classes/ApiErrorExtension.js
@@ -0,0 +1,12 @@
+/**
+ * An problem document extension according to RFC 7807
+ */
+class Extension {
+    /**
+     *
+     * @param {Object} extension
+     */
+    constructor(extension) {
+        this.extensionProperties = extension;
+    }
+}
