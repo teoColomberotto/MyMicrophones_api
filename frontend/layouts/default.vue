dd053e93cc343f81ce1d693a8de7fc31a6f7e8c4
teoColomberotto <teocolomberotto97@gmail.com>
2022-03-10 22:54:08 +0100

    add: Header & Basics components 2 test

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <the-header />
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+import TheHeader from '../components/templates/TheHeader.vue';
+
+export default {
+    components: {
+        'the-header': TheHeader,
+    },
+};
+</script>
+
+<style></style>
