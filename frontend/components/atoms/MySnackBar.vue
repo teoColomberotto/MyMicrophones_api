4b2710987789d80ceec65ab36a57c4a7659b3036
teoColomberotto <teocolomberotto97@gmail.com>
2022-03-12 03:06:59 +0100

    add: mySnackbar component

---
+++ frontend/components/atoms/MySnackBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div :class="type">
+        <slot name="message"></slot>
+        <slot name="action"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    props: {
+        type: { type: String, required: true, default: 'message' },
+        duration: { type: Number, required: false, default: 3000 },
+    },
+    mounted() {
+        setTimeout(() => {
+            this.remove();
+        }, this.duration);
+    },
+    methods: {
+        remove() {
+            this.$emit('removeAlert');
+        },
+    },
+};
+</script>
+
+<style lang="css" scoped>
+div {
+    margin: 2rem auto;
+    padding: 1rem;
+    max-width: 40rem;
+    border-radius: 12px;
+    background-color: var(--main-black);
+    color: var(--main-color);
+    display: flex;
+    align-content: center;
+    justify-content: center;
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,28 +1,28 @@
 <template>
     <div>
-        <profile-icon></profile-icon>
-        <my-text-input
+        <my-snackbar>
+            <template #message> Ops, something went wrong</template>
+        </my-snackbar>
+        <!-- <profile-icon></profile-icon> -->
+        <!-- <my-text-input
             type="password"
             v-model="email"
             label="Email"
             icon="fa-solid fa-user-tie"
             placeholder="Please input a valid email address"
             :options="options"
-        ></my-text-input>
+        ></my-text-input> -->
         <!-- <my-pagination :pageSize="20" @pageChanged="onPageChange" :total="140" :current-page="currentPage"></my-pagination> -->
     </div>
 </template>
 
 <script>
-import MyTextInputVue from '../components/atoms/MyTextInput.vue';
-import MyPaginationVue from '../components/molecules/MyPagination.vue';
-import MyProfileIconVue from '../components/molecules/MyProfileIcon.vue';
+import MySnackBarVue from '../components/atoms/MySnackBar.vue';
 
 export default {
     name: 'IndexPage',
     components: {
-        'profile-icon': MyProfileIconVue,
-        'my-text-input': MyTextInputVue,
+        'my-snackbar': MySnackBarVue,
         // 'my-pagination': MyPaginationVue,
     },
     data() {
